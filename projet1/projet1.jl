f5ea2874faa524ea32f8454f6227bde8ead2bde8
alexisbarreaux <67143881+alexisbarreaux@users.noreply.github.com>
2023-01-25 15:48:35 +0100

    Updated model

--- projet1/projet1.jl
+++ projet1/projet1.jl
@@ -2,6 +2,7 @@ using JuMP
 using CPLEX
 
 include("./constants.jl")
+include("./parser.jl")
 
 
 """
@@ -13,7 +14,7 @@ function neighbors(i::Int64, j::Int64)::Vector{Tuple{Int64, Int64}}
 end
 
 function returnAlphas(case::Int64)::Vector{Float64}
-    if case == 1:
+    if case == 1
         return [0.5 for _ in 1:6]
     elseif case == 2
         return vcat([0.9 for _ in 1:3],[0.5 for _ in 1:3])
@@ -26,14 +27,14 @@ function returnAlphas(case::Int64)::Vector{Float64}
     end
 end
 
-function reserveSolve(case::Int64, silent::Bool=true)::Any
+function reserveSolve(case::Int64, instance::String= "probabilities.txt",silent::Bool=false)::Any
     """
     """
     # Directly load data file
     # TODO parsing
-    include(PROJET_1_PATH * "\\" * inputFile)
-    n = 10
     alphas = returnAlphas(case)
+    a = cost()
+    p, n = parser(PROJET_1_PATH * "\\" * instance)
 
     reserve = [(i,j) for i in 2:n+1 for j in 2:n+1]
     border = vcat([(i,j) for i in 1:n+2 for j in [1, n+2]], [(i,j) for i in [1,n+2] for j in 2:n+1])
@@ -68,4 +69,25 @@ function reserveSolve(case::Int64, silent::Bool=true)::Any
     @constraint(model, [k in dangerSpecies], sum(c[i,j]*log(1-p[k,i,j]) for (i,j) in reserve) <= log(1- alphas[k]))
     @constraint(model, [k in communeSpecies], sum(x[i,j]*log(1-p[k,i,j]) for (i,j) in reserve) <= log(1- alphas[k]))
 
+    
+    optimize!(model)
+    feasibleSolutionFound = primal_status(model) == MOI.FEASIBLE_POINT
+    isOptimal = termination_status(model) == MOI.OPTIMAL
+
+    if feasibleSolutionFound
+        x_val = JuMP.value.(x)
+        c_val = JuMP.value.(c)
+        dangerSpecies = [1,2,3]
+        communeSpecies = [4,5,6]
+        for k in dangerSpecies
+            println("Specie " * string(k) * " survival rate " * string(1 - prod([1- p[k,i,j]*c_val[i,j] for (i,j) in reserve])))
+        end
+        for k in communeSpecies
+            println("Specie " * string(k) * " survival rate " * string(1 - prod([1- p[k,i,j]*x_val[i,j] for (i,j) in reserve])))
+        end
+        return JuMP.objective_value(model)
+    else
+        println("Problem is not feasible !!!")
+        return
+    end
 end
