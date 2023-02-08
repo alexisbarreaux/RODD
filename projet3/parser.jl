aa421e2f24cab615f84610843887fbed57a78920
Gonzague <gonzague.bt@gmail.com>
2023-02-08 15:17:01 +0100

    parser projet3 fonctionnel

---
+++ projet3/parser.jl
@@ -0,0 +1,27 @@
+
+
+function parser(inst::String="data/DivGenetique.txt")
+    f = open(inst)
+    for i in 1:5
+        readline(f)
+    end
+    M = zeros(8,5,2)
+    line = readline(f)
+    while line != "\t"
+        if line==""
+            line = readline(f)
+        end
+        if line==""
+            break
+        end
+        line = split(line, " ")
+        println(line)
+        M[parse(Int64,line[1]),parse(Int64,line[3]), parse(Int64,line[5])] += 0.5
+        line = readline(f)
+    end
+    return 8, 5, 2, M
+end
+    
+
+
+    
