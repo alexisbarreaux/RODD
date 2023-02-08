ed6d535a7e749b7bda4ddcca99a51cd983788e4a
alexisbarreaux <67143881+alexisbarreaux@users.noreply.github.com>
2023-02-08 16:42:14 +0100

    Corrected genes generation

--- projet3/generateDatas.jl
+++ projet3/generateDatas.jl
@@ -1,10 +1,21 @@
 
 function generateM(nbInd::Int64=8, nbLocus::Int64=5, nbAllele::Int64=2)
     M = zeros(nbInd,nbLocus, nbAllele)
-    for i in 1:nbInd
-        for j in 1:nbLocus
-            for k in 1:nbAllele
-                M[i,j,k] = rand([0, 0.5, 1])
+    for p in 1:nbInd
+        for i in 1:nbLocus
+            allelesLeft = nbAllele
+            for j in 1:nbAllele
+                if allelesLeft == 0
+                    break
+                else
+                    if j == nbAllele
+                        allelesOfTypeJ = allelesLeft
+                    else
+                        allelesOfTypeJ = rand(1:allelesLeft)
+                    end
+                    M[p,i,j] = allelesOfTypeJ / nbAllele
+                    allelesLeft -= allelesOfTypeJ
+                end
             end
         end
     end
