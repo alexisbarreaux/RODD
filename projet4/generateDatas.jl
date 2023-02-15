6bfdd61e4eed2882ec95364394ecedf6333dc7e2
Gonzague <gonzague.bt@gmail.com>
2023-02-15 15:45:25 +0100

    ajout instance 2

--- projet4/generateDatas.jl
+++ projet4/generateDatas.jl
@@ -10,3 +10,16 @@ function buildtij(n::Int64, m::Int64)
     end
     return t
 end
+
+function instance2()
+    t = [ 
+        0 0 0 0 0 0 0 ;
+        0 10 10 10 1 10 0 ;
+        0 10 10 1 1 10 0 ;
+        0 10 10 1 10 10 0 ;
+        0 1 10 10 10 10 0 ;
+        0 1 10 10 10 10 0 ;
+        0 0 0 0 0 0 0
+    ]
+    return t
+end
